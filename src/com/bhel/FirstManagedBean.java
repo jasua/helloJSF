ccce6152d716f0083bfc6834bab53d3b569122ca
naggu <naggu@192.168.0.4>
2017-10-24 07:27:44 +0530

    First commit

---
+++ src/com/bhel/FirstManagedBean.java
@@ -0,0 +1,27 @@
+package com.bhel;
+
+import javax.annotation.ManagedBean;
+
+//author: nagaraju
+
+@ManagedBean
+public class FirstManagedBean {
+	
+	private String firstName;
+	private String lastName;
+	
+	public FirstManagedBean(){}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
